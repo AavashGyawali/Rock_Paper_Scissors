0de6e87688407eb5157849ff163d4ae36152544c
Aavash Gyawali <aavash.076bct003@gmail.com>
2022-12-23 09:58:08 +0545

    Comment added

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,20 @@
 import random
 enter=True
+# initializing the score
 score_user =0
 score_computer =0
+# running the loop until the user enters 'q'
 while enter!='q':
+    # possible inputs
     possible_action= ['rock','paper','scissors']
+    # input to the user
     user_action1= input("Enter 'rock','paper'or'scissors' ")
     user_action= user_action1.lower()
+    # condition if user gives incorrect input
     if user_action1 not in possible_action:
         print ("you did not typed correctly")
         continue
+    # generating random input by computer
     computer_action =random.choice(possible_action)
     print(f"You choice is {user_action} and computer's choice is {computer_action}")
     if user_action == computer_action:
@@ -35,6 +41,7 @@ while enter!='q':
             print("Rock smashes scissors! You lose.")
             score_computer=score_computer + 1
     enter = input("Enter q to exit else enter anything to continue \n")
-
+    
+# Final score 
 print(f"Your score is {score_user} \n Computer's score is {score_computer} ")
 
